caa81cc81ace7e7db78a427a30f390f07d387e02
Arvin Wiyono <arvinwiyono@gmail.com>
2016-01-27 17:04:26 +0700

    Add orders' total

--- section-6/app/controllers/allOrdersController.js
+++ section-6/app/controllers/allOrdersController.js
@@ -3,14 +3,23 @@
 	var AllOrdersController = function($scope, $log, ordersService, appSettings){
 
 		$scope.orders = [];
+		$scope.ordersTotal = 0.0;
 		$scope.appSettings = appSettings;
 
 		ordersService.getOrders()
 			.then(function(response){
 				$scope.orders = response.data;
+				getOrdersTotal();
 			}, function errorCallback(response){
 				$log.log(response.status +' - ' + response.data.error);
 			});
+
+		var getOrdersTotal = function(){
+			for(var i = 0; i < $scope.orders.length; i++){
+				$scope.ordersTotal += $scope.orders[i].total;
+			}
+			$scope.totalType = ($scope.ordersTotal > 100) ? 'success' : 'danger';
+		}
 	};
 
 	AllOrdersController.$inject = ['$scope', '$log', 'ordersService', 'appSettings'];
