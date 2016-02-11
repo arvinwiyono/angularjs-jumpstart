68c96dbb34058ed1ddbb2e57c92f45a114a803cf
Arvin Wiyono <arvinwiyono@gmail.com>
2016-02-11 22:38:51 +0700

    Link function in custom directive

---
+++ section-8/linkDemoDirective.js
@@ -0,0 +1,24 @@
+(function(){
+
+	var linkDemo = function(){
+		return {
+			restrict: 'A',
+			link: function(scope, elem, attrs){
+				elem.on('click', function(){
+					elem.html('You clicked me');
+				});
+
+				elem.on('mouseenter', function(){
+					elem.css('background-color', 'yellow');
+				});
+
+				elem.on('mouseleave', function(){
+					elem.css('background-color', 'grey');
+				});
+			}
+		};
+	};
+
+	angular.module('directiveModule')
+		.directive('linkDemo', linkDemo);
+})();
